6d055820e660aaa315622a9350dee990003bf9d1
Yura Subotinov <crapmindd@gmail.com>
2025-09-22 14:32:46 +0300

    chore: initial commit with project structure

---
+++ data-api/src/test/java/dev/subotinov/dataapi/DataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.subotinov.dataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
