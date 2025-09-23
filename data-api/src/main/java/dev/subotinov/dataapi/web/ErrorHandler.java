505355967d78c05b62b6bde57ee29b48b363a5b3
Yura Subotinov <crapmindd@gmail.com>
2025-09-23 12:17:12 +0300

    feat(data-api): add global error handler with ProblemDetail

---
+++ data-api/src/main/java/dev/subotinov/dataapi/web/ErrorHandler.java
@@ -0,0 +1,35 @@
+package dev.subotinov.dataapi.web;
+
+import dev.subotinov.dataapi.security.InternalTokenException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(InternalTokenException.class)
+    public ProblemDetail handleInternalToken(InternalTokenException ex) {
+        var pd = ProblemDetail.forStatus(HttpStatus.FORBIDDEN);
+        pd.setDetail(ex.getMessage());
+        return pd;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidation(MethodArgumentNotValidException ex) {
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pd.setTitle("Validation Failed");
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fe : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fe.getField(), fe.getDefaultMessage());
+        }
+        pd.setProperty("errors", errors);
+        return pd;
+    }
+}
